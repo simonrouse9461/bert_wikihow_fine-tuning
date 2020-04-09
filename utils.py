da88ca07e6ccc690c83b76ae095ff64d79a68312
Chuhan Feng <simonrouse9461@gmail.com>
2020-04-09 03:25:01 +0000

    finish training

--- utils.py
+++ utils.py
@@ -1,33 +1,46 @@
 class Logger:
-    
+
     def __init__(self, file, 
-                 log_fn=lambda x: x, 
-                 aggre_fn=None, 
+                 map_fn=lambda x: x, 
+                 reduce_fn=None, 
+                 header=None,
                  overwrite=False, 
                  period=1):
         self.file = file
         self.fout = None
         self.mode = 'w' if overwrite else 'a'
-        self.log_fn = log_fn
-        self.aggre_fn = aggre_fn
+        self.map_fn = map_fn
+        self.reduce_fn = reduce_fn
+        self.header = header
         self.period = period
         self.counter = 0
         self.history = []
-        
+
     def __enter__(self):
-        self.fout = open(self.file, self.mode)
+        self.open()
         return self
-        
+
     def __exit__(self, type, value, traceback):
+        self.close()
+
+    def open(self):
+        self.fout = open(self.file, self.mode)
+        if self.header is not None:
+            self.log(self.header)
+
+    def close(self):
         self.fout.close()
-        
+
+    def log(self, *msg, **kwargs):
+        print(*msg, file=self.fout, flush=True, **kwargs)
+
     def step(self, *args, **kwargs):
         self.counter += 1
-        if self.aggre_fn is not None:
-            self.history.append(self.log_fn(*args, **kwargs))
+        if self.reduce_fn is not None:
+            self.history.append(self.map_fn(*args, **kwargs))
         if self.counter % self.period == 0:
-            value = (self.log_fn(*args, **kwargs) 
-                     if self.aggre_fn is None
-                     else self.aggre_fn(self.history))
-            print(value, file=self.fout, flush=True)
+            value = (self.map_fn(*args, **kwargs) 
+                     if self.reduce_fn is None
+                     else self.reduce_fn(self.history))
+            self.log(value)
             self.history.clear()
